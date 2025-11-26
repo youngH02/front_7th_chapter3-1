8693a97cbf7aea80d9b974974f2773d0fd99251d
jyoung02up <>
2025-11-26 16:35:00 +0900

    table UI 추가

---
+++ packages/after/src/components/common/DataTable.tsx
@@ -0,0 +1,52 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { type ReactNode } from "react";
+
+export type TColumn<T> = {
+  header: string;
+  key: string;
+  width?: string;
+  render?: (item: T) => ReactNode;
+};
+
+interface IProps<T> {
+  columns: TColumn<T>[];
+  data: T[];
+}
+
+const DataTable = <T,>({ columns, data }: IProps<T>) => {
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            {columns.map((col) => (
+              <TableHead key={col.key} className={col.width}>
+                {col.header}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {columns.map((col) => (
+                <TableCell key={col.key}>
+                  {col.render ? col.render(row) : (row as any)[col.key]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default DataTable;
