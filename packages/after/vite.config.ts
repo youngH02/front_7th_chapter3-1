dff00ebcb985d6ea210a03b5ca54132b2daf5c62
jyoung02up <>
2025-11-27 22:32:53 +0900

    배포 base url 수정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/front_7th_chapter3-1/",
   // @ts-ignore - vite version conflict
   plugins: [react()],
   resolve: {
